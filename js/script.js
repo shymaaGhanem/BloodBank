3facd67aab7d1404a216f6f1becb02ee8c75ac5f
shymaaGhanem <shimaa44ghanem@gmail.com>
2020-06-11 22:43:59 +0200

    about section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+
+//about
+  var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+  $(".fade").addClass('animate__animated  animate__rotateInDownLeft  animate__rotateInDownRight animate__delay-1s',1000,function(){
+
+      $(".fade").each(function() {
+          /* Check the location of each desired element */
+          var objectBottom = $(this).offset().top + $(this).outerHeight();
+          
+          /* If the element is completely within bounds of the window, fade it in */
+          if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+            if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+          } else { //object goes out of view (scrolling up)
+            if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+          }
+        });
+  })
+
+
+
+
+
+
+
+
+
+
 // testimonial starting 
 $(document).ready(function () {
 
