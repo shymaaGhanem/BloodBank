d44b2b624d3959c09bceca1d3e0aaf3aa13ea01d
nermeenkelila <nermeen_kelila@hotmail.com>
2020-06-15 15:04:50 +0200

    Add volunteers page and blood requests page

---
+++ js/searchpages.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        items:1,
+        rtl:true,
+        center: true,
+        nav:true,
+        navText :[" السابق" , " التالى "],
+      
+    });
+  });
